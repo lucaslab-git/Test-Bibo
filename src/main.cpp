ad6eb9b478961fa49ad03a9a14e43ad8463c95fb
Luca's Lab <92208704+lucaslab-git@users.noreply.github.com>
2022-11-29 18:06:50 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+//-----Test-Bibo-----//
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+void test_sensor() {
+  //Code, wenn Defekt == False
+}
+
+void test_motor(){
+  //Code control motor
+}
